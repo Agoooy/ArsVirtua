d0c072012a35ca5d749f5c5c3977137bae84b38d
Yogaa12 <you@example.com>
2024-07-18 21:46:15 +0700

    chore: Update company name in TicketNavbar component

--- app/tickets/TicketNavbar.tsx
+++ app/tickets/TicketNavbar.tsx
@@ -6,7 +6,7 @@ const TicketNavbar = ({className}:{className?:string}) => {
     <nav className={`w-full flexBetween ss:px-4 sm:px-8 py-[12px] bg-white ${className}`}>
       <Link href={`/`} className="flexCenter gap-1 text-blue-600">
         <HiMiniBuildingLibrary className="text-[26px] -mt-[3px]"/>
-        <span className="text-[20px] ss:text-[22px] font-bold">EuroPark</span>
+        <span className="text-[20px] ss:text-[22px] font-bold">ArsVirtua</span>
       </Link>
       <Link href={`/`} className="text-blue-600 hidden ss:block">Beranda</Link>
     </nav>
